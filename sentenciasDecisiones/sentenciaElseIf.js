f16dacd6c1b37c0adb4c45a05c8f46f8c7ffee80
inmutz <isaiasariza1686@gmail.com>
2023-12-23 01:09:50 -0500

    sentencias If Else1

---
+++ sentenciasDecisiones/sentenciaElseIf.js
@@ -0,0 +1,14 @@
+//Sentecias if 
+let miNumero = -10; 
+
+if (miNumero > 0){
+    console.log(`Valor posito ${miNumero}`); 
+}
+
+else if (miNumero < 0){
+    console.log(`valor negativo ${miNumero}`); 
+}
+
+else {
+    console.log(`Valor cero o negativo ${miNumero}`);
+}
